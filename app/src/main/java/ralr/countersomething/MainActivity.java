2e257f82a96e1ebfd7d8e4edf5b56edeefd546d7
rulogarcillan <temporal>
2015-06-22 17:06:23 +0200

    Initial commit

--- app/src/main/java/ralr/countersomething/MainActivity.java
+++ app/src/main/java/ralr/countersomething/MainActivity.java
@@ -1,19 +1,137 @@
 package ralr.countersomething;
 
-import android.support.v7.app.ActionBarActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt_count;
+    ImageButton ic_restore, ic_edit;
+    RelativeLayout up;
+    int total = 0;
+    int currentapiVersion;
+    SharedPreferences.Editor editor;
+    SharedPreferences prefs;
+    String KEY_PREF_COUNT = "TOTAL";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        up = (RelativeLayout) findViewById(R.id.up);
+        txt_count = (TextView) findViewById(R.id.txt_count);
+        ic_edit = (ImageButton) findViewById(R.id.ic_edit);
+        ic_restore = (ImageButton) findViewById(R.id.ic_restore);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //  getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // getSupportActionBar().setHomeButtonEnabled(true);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = prefs.edit();
+
+        txt_count.setText(Integer.toString(prefs.getInt(KEY_PREF_COUNT,0)));
+
+
+        currentapiVersion = android.os.Build.VERSION.SDK_INT;
+
+
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = Integer.parseInt(txt_count.getText().toString()) + 1;
+                editor.putInt(KEY_PREF_COUNT, total);
+                editor.commit();
+                if (currentapiVersion >= Build.VERSION_CODES.LOLLIPOP) {
+                    efectoDentro();
+                } else {
+                    txt_count.setText(Integer.toString(total));
+                }
+
+
+
+            }
+        });
+
+        ic_restore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = 0;
+                editor.putInt(KEY_PREF_COUNT, total);
+                editor.commit();
+                if (currentapiVersion >= Build.VERSION_CODES.LOLLIPOP) {
+                    efectoDentro();
+                } else {
+                    txt_count.setText(Integer.toString(total));
+                }
+            }
+        });
     }
 
+
+    private void efectoFuera() {
+
+
+        int cx = (txt_count.getLeft() + txt_count.getRight()) / 2;
+        int cy = (txt_count.getTop() + txt_count.getBottom()) / 2;
+
+
+        int finalRadius = Math.max(txt_count.getWidth(), txt_count.getHeight());
+
+
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(txt_count, cx, cy, 0, finalRadius);
+
+
+        txt_count.setVisibility(View.VISIBLE);
+        //anim.setDuration(300);
+        anim.start();
+    }
+
+    private void efectoDentro() {
+        // get the center for the clipping circle
+        // get the center for the clipping circle
+        int cx = (txt_count.getLeft() + txt_count.getRight()) / 2;
+        int cy = (txt_count.getTop() + txt_count.getBottom()) / 2;
+
+
+        int initialRadius = txt_count.getWidth();
+
+
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(txt_count, cx, cy, initialRadius, 0);
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                txt_count.setVisibility(View.INVISIBLE);
+                txt_count.setText(Integer.toString(total));
+                efectoFuera();
+            }
+        });
+
+
+        // anim.setDuration(300);
+        anim.start();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
