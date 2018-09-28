58a602aa36e4c005b2cf2c67a49743c88f123b86
rulogarcillan <raulrcs@gmail.com>
2018-09-28 16:54:34 +0200

    [FEAT] Adding privacy policy

--- app/src/main/java/ralr/countersomething/BaseActivity.java
+++ app/src/main/java/ralr/countersomething/BaseActivity.java
@@ -34,7 +34,7 @@ import androidx.appcompat.app.AppCompatActivity;
 /**
  * Created by raul.rodriguezconcep on 20/02/15.
  */
-public class BaseActivity  extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,38 +51,14 @@ public class BaseActivity  extends AppCompatActivity {
             case R.id.action_settings:
                 break;
             case R.id.puntuar:
-
                 LanzaRate();
-
                 break;
-
             case R.id.more:
-
                 LanzaMore();
-
                 break;
-          /*  case R.id.changelog:
-
-                new LanzaChangelog(BaseActivity.this).getFullLogDialog().show();
+            case R.id.privacy_policy:
+                openPrivacy();
                 break;
-            case R.id.license:
-                //Create an intent with context and the Activity class
-
-                new Libs.Builder()
-                        //Pass the fields of your application to the lib so it can find all external lib information
-                        .withFields(R.string.class.getFields())
-                        .withVersionShown(true)
-                        .withLicenseShown(true)
-                        .withAutoDetect(true)
-                        .withLibraries("sqliteassethelper")
-                        .withActivityTitle(getResources().getString(R.string.license))
-                        .withAboutDescription(getResources().getString(R.string.escrita) + "<br/><br/><b>License GNU GPL V3.0</b><br/><br/><a href=\"https://github.com/rulogarcillan/PainLog\">Project in Github</a>")
-                        .withActivityTheme(R.style.AppTheme)
-                                //start the activity
-                        .start(this);
-                break;*/
-
-
             default:
                 onBackPressed();
         }
@@ -94,32 +70,20 @@ public class BaseActivity  extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("market://details?id=ralr.countersomething"));
         startActivity(intent);
-      //  overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        //  overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+    }
+
+    private void openPrivacy() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://info.tuppersoft.com/privacy/privacy_policy_counter.html"));
+        startActivity(browserIntent);
     }
 
 
     private void LanzaMore() {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:Raúl R."));
         startActivity(intent);
-     //   overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        //   overridePendingTransition(R.anim.fadein,R.anim.fadeout);
     }
-
-
-  /*  public static class LanzaChangelog extends ChangeLog {
-
-
-        public static final String DEFAULT_CSS =
-
-                "body {                                                           " + "	font-family: Verdana, Helvetica, Arial, sans-serif;   " + "	font-size: 11px;                                      " + "	color: #000000;                                       " + "	background-color: #ffffff;                            " + "	margin: 0px;                                          " + "	padding: 0px;                                         " + "}                                                        "
-                        + "h1 {                                                     " + "	font-size: 14px;                                      " + "	font-weight: bold;                                    " + "	text-transform: uppercase;                            " + "	color: #000000;                                       " + "	margin: 0px;                                          " + "	padding: 10px 0px 0px 8px;                            " + "}                                                        "
-                        + "h2 {                                                     " + "	font-size: 10px;                                      " + "	color: #999999;                                       " + "	font-weight: normal;                                  " + "	margin: 0px 0px 0px 8px;                              " + "	padding: 0px;                                         " + "}                                                        " + "ul {                                                     "
-                        + "	margin: 0px 0px 10px 15px;                            " + "	padding-left: 15px;                                " + "	padding-top: 8px;                                     " + "	list-style-type: square;                              " + "	color: #999999;                                       " + "}";
-
-        public LanzaChangelog(Context context) {
-            super(new ContextThemeWrapper(context, R.style.AppTheme), DEFAULT_CSS);
-        }
-    }*/
-
 }
 
 
